e81122ad5369bb7d96a9f11310d966d58af59054
AaronNicolalde <eduanico@espol.edu.ec>
2022-05-25 01:31:49 -0500

    Customer controller test update

--- src/test/java/com/eduanico/resourceserv/web/controller/CustomerControllerTest.java
+++ src/test/java/com/eduanico/resourceserv/web/controller/CustomerControllerTest.java
@@ -1,30 +1,55 @@
 package com.eduanico.resourceserv.web.controller;
 
+import com.eduanico.resourceserv.service.impl.CustomerServiceImpl;
+import com.eduanico.resourceserv.web.model.Customer;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.security.Principal;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
 @DisplayName("Customer controller Tests")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class CustomerControllerTest {
 
-    @Autowired
+    @Spy
+    @InjectMocks
     CustomerController customerController;
+
+    @Mock
+    CustomerServiceImpl customerService;
+
+    @Mock
+    private Principal principal;
+
+    private String defaultName = "name";
+
+    @Mock
+    private ResponseEntity responseEntity;
 
     @Test
     @DisplayName("Add customer returns api response when successful")
     void addCustomer_WhenSuccessful() {
-//        assertThat(customerController.addCustomer(new Customer("eduanico"))).isInstanceOf(ResponseEntity.class);
+        when(principal.getName()).thenReturn(defaultName);
+        Customer customer = new Customer(principal.getName());
+        when(customerService.createCustomer(new Customer(anyString()))).thenReturn(customer);
+
+        assertThat(customerController.addCustomer(principal)).isInstanceOf(ResponseEntity.class);
     }
 
     @Test
